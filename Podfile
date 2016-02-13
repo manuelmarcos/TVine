38f742548c2576cdabe0d9ec77268002bc67ae3e
Manuel Marcos Regalado <manuel@ribot.co.uk>
2016-02-13 19:37:48 +0000

    First commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+# Podfile
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :tvos, '9.0'
+use_frameworks!
+
+pod 'Alamofire', '~> 3.0'
---
+++ TVine/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  TVine
+//
+//  Created by Manuel Marcos Regalado on 13/02/2016.
+//  Copyright © 2016 Manuel Marcos Regalado. All rights reserved.
+//
+
+import Foundation
